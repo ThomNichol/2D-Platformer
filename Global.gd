f2c1fc54be631cff85d9bc5d7bc708aa91493feb
ThomNichol <thonicho@iu.edu>
2023-12-04 01:07:33 -0500

    Project 4 WIP (Almost Done!)

--- Global.gd
+++ Global.gd
@@ -12,3 +12,9 @@ func update_coins(coins):
 	var hud = get_node_or_null("/root/Coins")
 	if hud != null:
 		hud.update_score()
+		
+func end_game(success):
+	if success:
+		get_tree().change_scene_to_file("res://Game_End.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Game_End.tscn")
